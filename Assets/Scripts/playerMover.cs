50bbb368408fbf2a4829e82d412b0583025ef59f
Xaaldir <reubencoll@gmail.com>
2024-01-13 17:39:36 -0500

    made some terrain and some rigidbodies

--- Assets/Scripts/playerMover.cs
+++ Assets/Scripts/playerMover.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class playerMover : MonoBehaviour
 {
     // changes how fast the player accelerates
-    [SerializeField] float weight = 0.05f;
+    [SerializeField] float weight = 5.71f;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("I am working");
         // transform.Translate(0,0,1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x_velocity = Input.GetAxis("Horizontal") * weight;
-        float z_velocity = Input.GetAxis("Vertical") * weight;
+        float x_velocity = Input.GetAxis("Horizontal") * weight * Time.deltaTime;
+        float z_velocity = Input.GetAxis("Vertical") * weight * Time.deltaTime;
 
         transform.Translate(x_velocity, 0f, z_velocity);
     }
